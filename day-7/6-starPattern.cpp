a354618ced1885105d3f856db98942ad7485768b
krishnkant nagda <kknagda488@gmail.com>
2023-10-11 14:49:05 +0530

    day 7 added

--- day-7/6-starPattern.cpp
+++ day-7/6-starPattern.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
 using namespace std;
-
 int main(){
     int i,j;
     for(i=1;i<=4;i++){
-        for(j=i;j>=1;j--){
-            cout<<j<<" ";
+        for(j=4;j>=i;j--){
+            cout<<"*";
         }
         cout<<endl;
     }
--- day-7/7-numPattern.cpp
+++ day-7/7-numPattern.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(){
     int i,j;
-    for(i=1;i<=4;i++){
-        for(j=i;j>=1;j--){
-            cout<<j<<" ";
+    for(i=1;i<=5;i++){
+        for(j=5;j>=5-i+1;j--){
+            cout<<j;
         }
         cout<<endl;
     }
